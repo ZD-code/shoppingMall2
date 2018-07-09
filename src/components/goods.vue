be0a0f3273360fb2f2c6aba17298ff13a5db45af
brand <zhangdong891217@126.com>
2018-07-09 11:02:40 +0800

    继续完善详情页

--- src/components/goods.vue
+++ src/components/goods.vue
@@ -1,14 +1,33 @@
 <template>
-    <div>
-<div class="navbar-div">
-    <van-nav-bar title="商品详情" left-text="返回" left-arrow @click-left="onClickLeft"></van-nav-bar>
-</div>
-<div class="topimage-div">
-    <img :src="goodsInfo.IMAGE1" width="100%">
-</div>
-<div class="goods-detail" v-html="goodsInfo.DETAIL">
+    <div style="padding-bottom:65px;">
+        <div class="navbar-div">
+            <van-nav-bar title="商品详情" left-text="返回" left-arrow @click-left="onClickLeft"></van-nav-bar>
+        </div>
+        <div class="topimage-div">
+            <img :src="goodsInfo.IMAGE1" width="100%">
+        </div>
+        <div class="goods-name" v-text="goodsInfo.NAME"></div>
+        <div class="goods-price">价格：￥{{goodsInfo.PERSENT_PRICE | moneyFilter}}</div>
+        <div>
+            <van-tabs>
+                <van-tab title="商品详情">
+                    <div class="detail" v-html="goodsInfo.DETAIL">
 
-</div>
+                    </div>
+                </van-tab>
+                <van-tab title="评价">
+                    正在制作中……
+                </van-tab>
+            </van-tabs>
+        </div>
+        <div class="goods-bottom">
+            <div>
+                <van-button size="large">加入购物车</van-button>
+            </div>
+            <div>
+                <van-button size="large" type="primary">直接购买</van-button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -16,6 +35,7 @@
 import axios from "axios";
 import url from "@/apiConfig";
 import { Toast } from "vant";
+import {toMoney} from "@/filter/money";
     export default {
         data(){
             return {
@@ -27,6 +47,11 @@ import { Toast } from "vant";
             this.goodsId = this.$route.query.goodsId;
             console.log(this.goodsId)
            this.getGoodsInfo();
+        },
+        filters:{
+            moneyFilter(money){
+                return toMoney(money);
+            }
         },
         methods:{
             onClickLeft(){
@@ -58,5 +83,29 @@ import { Toast } from "vant";
 </script>
 
 <style scoped>
+ .detail{
+     font-size:0px;
+    }
+    .goods-name{
+        background-color: #fff;
+    }
+    .goods-price{
+        background-color: #fff;
+    }
+    .goods-bottom{
+    position: fixed;
+    bottom:0px;
+    left:0px;
+    background-color: #FFF;
+    width:100%;
+
+    display: flex;
+    flex-direction: row;
+    flex-flow: nowrap;
+}
+.goods-bottom > div{
+    flex:1;
+    padding:5px;
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { Button, Row, Col, Icon, Swipe, SwipeItem, Search, Lazyload, List, NavBar, Field } from 'vant'
+import { Button, Row, Col, Icon, Swipe, SwipeItem, Search, Lazyload, List, NavBar, Field, Tabs, Tab } from 'vant'
 
 Vue.config.productionTip = false
-Vue.use(Button).use(Row).use(Col).use(Icon).use(Swipe).use(SwipeItem).use(Search).use(Lazyload).use(List).use(NavBar).use(Field)
+Vue.use(Button).use(Row).use(Col).use(Icon).use(Swipe).use(SwipeItem).use(Search).use(Lazyload).use(List).use(NavBar).use(Field).use(Tabs).use(Tab)
     /* eslint-disable no-new */
 new Vue({
     el: '#app',
